26803507de188a15cfbc1b79869260201b9fc1e5
tony <70nybl4nc0@mail.com>
2019-12-04 21:36:51 -0500

    Simple method implementation

--- chord/chord.py
+++ chord/chord.py
@@ -1,48 +1,41 @@
 from chord.node import Node
 from utils import aid
+import debug.logger as log
 
 
 class Chord:
     'Interface for handle chord ring'
+
     def __init__(self, id, ip, port):
-        for i in range(1, 1023 - port):
-            try:
-                self.node = Node(id, ip, port + i) 
-                break
-            except:
-                raise Exception('Se falló asignando la dirección')
-        
+        self.node = Node(id, ip, port)
 
     def __del__(self):
         'kill local node and release resources'
-        pass 
-    
-    def add(self, key, value):
+        del(self.node)
+
+    def add(self, key:int, value):
         "add a new key,value into the chord ring"
         self.node.save(key, value)
 
-
-    def get(self, key):
+    def get(self, key:int):
         'get value of a key located in Chord ring'
-        pass
-    
+        self.node.load(key)
 
     def get_local_values(self):
         'get local node values'
-        pass
-
+        return list(self.node.data.values())
 
     def get_values(self):
         'gets all the values stored in the ring'
+        pass
 
-
-    def delete_key(self, key):
+    def delete_key(self, key:int):
         'deletes a given key'
-        pass
+        self.node.delete(key)
 
-    def join(self, other):
-        'joins two chords together (really only one node)'
-        pass
+    def join(self, uri):
+        'joins this node to a chord ring using this node (really only one node)'
+        self.node.start_serving(uri=uri)
 
     def get_value(self):
         'gets a random value from the chord ring'
